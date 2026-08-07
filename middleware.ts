66ec9d4f745899de29bef24871417b2b72dcd41d
Kesar Karale <kesarkarale1@gmail.com>
2026-08-07 20:00:06 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,59 +1,185 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { createServerClient } from "@supabase/ssr";
 
+
 export async function middleware(request: NextRequest) {
-  let response = NextResponse.next();
+
+
+  let response = NextResponse.next({
+    request,
+  });
+
+
 
   const supabase = createServerClient(
+
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+
+
     {
+
       cookies: {
+
         getAll() {
+
           return request.cookies.getAll();
+
         },
+
 
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            response.cookies.set(name, value, options)
+
+
+          cookiesToSet.forEach(({ name, value }) => {
+
+            request.cookies.set(
+              name,
+              value
+            );
+
+          });
+
+
+
+          response = NextResponse.next({
+
+            request,
+
+          });
+
+
+
+          cookiesToSet.forEach(
+            ({ name, value, options }) => {
+
+
+              response.cookies.set(
+
+                name,
+
+                value,
+
+                options
+
+              );
+
+
+            }
           );
+
+
         },
+
+
       },
+
+
     }
+
   );
 
+
+
+
   const {
-    data: { user },
+    data:{
+      user
+    }
+
   } = await supabase.auth.getUser();
+
+
+
 
   const pathname = request.nextUrl.pathname;
 
+
+
   const publicRoutes = [
+
     "/",
+
     "/login",
+
     "/register",
-    "/forgot-password",
+
+    "/forgot-password"
+
   ];
 
-  if (!user && !publicRoutes.includes(pathname)) {
-    return NextResponse.redirect(new URL("/login", request.url));
+
+
+
+  if(
+
+    !user &&
+
+    !publicRoutes.includes(pathname)
+
+  ){
+
+    return NextResponse.redirect(
+
+      new URL("/login",request.url)
+
+    );
+
   }
 
-  if (user && (pathname === "/login" || pathname === "/register")) {
+
+
+
+  if(
+
+    user &&
+
+    (
+
+      pathname === "/login" ||
+
+      pathname === "/register"
+
+    )
+
+  ){
+
     return NextResponse.redirect(
-      new URL("/student/dashboard", request.url)
+
+      new URL("/student/dashboard",request.url)
+
     );
+
   }
+
+
+
 
   return response;
+
+
 }
 
+
+
+
 export const config = {
-  matcher: [
+
+
+  matcher:[
+
     "/admin/:path*",
+
     "/student/:path*",
+
     "/login",
+
     "/register",
-    "/forgot-password",
+
+    "/forgot-password"
+
   ],
+
+
 };
