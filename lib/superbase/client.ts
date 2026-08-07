be28d223674a6da55110398dc24b478168f5eb6d
Kesar Karale <kesarkarale1@gmail.com>
2026-08-07 19:38:28 +0530

    Update client.ts

--- lib/superbase/client.ts
+++ lib/superbase/client.ts
@@ -1,11 +1,14 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 
-export function createClient(){
+export function createClient() {
 
- return createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
- );
+  return createBrowserClient(
+
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+
+  );
 
 }
